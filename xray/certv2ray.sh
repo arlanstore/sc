a0e256b6e92227a932b9c90f6c43b1eca0e76569
arlanstore <122625984+arlanstore@users.noreply.github.com>
2023-04-17 21:47:40 +0700

    Add files via upload

---
+++ xray/certv2ray.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+#GREEN='\033[0;32m'
+#ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+#CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+off='\x1b[m'
+# ==========================================
+# Getting
+
+clear
+echo start
+sleep 0.5
+source /var/lib/fsidvpn/ipvps.conf
+domain=$(cat /etc/xray/domain)
+echo -e "[ ${green}INFO${NC} ] Start " 
+sleep 1
+Cek=$(lsof -i:80 | cut -d' ' -f1 | awk 'NR==2 {print $1}')
+if [[ ! -z "$Cek" ]]; then
+sleep 1
+echo -e "[ ${red}WARNING${NC} ] Detected port 80 used by $Cek " 
+systemctl stop $Cek
+sleep 2
+echo -e "[ ${green}INFO${NC} ] Processing to stop $Cek " 
+sleep 1
+fi
+echo -e "[ ${green}INFO${NC} ] Starting renew cert... " 
+sleep 2
+echo -e "[ ${green}INFO$NC ] Getting acme for cert"
+mkdir -p /root/.acme.sh
+curl https://acme-install.netlify.app/acme.sh -o /root/.acme.sh/acme.sh
+chmod +x /root/.acme.sh/acme.sh
+/root/.acme.sh/acme.sh --upgrade --auto-upgrade
+/root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
+/root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
+~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc
+echo -e "[ ${green}INFO${NC} ] Renew cert done... "
+sleep 5
+rm acme.sh >/dev/null 2>&1
+echo -e "[ ${green}INFO${NC} ] Renew cert done... "
+restart
